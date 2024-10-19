96266a59af6ce0ab1d93acea14e036a8ca6fe063
Thomas <thomas.huber@solve.ch>
2024-10-19 20:39:25 +0200

    Working on upgrade

--- upgrade.ts
+++ upgrade.ts
@@ -1,18 +1,26 @@
-import { parse } from '@std/path/parse';
+import { greaterThan, parse, type SemVer } from '@std/semver';
 
 import { runner } from './run.ts';
 import { VERSION } from './version.ts';
 import type { JsrMeta } from './types.ts';
 
-const apiUrl = 'https://jsr.io/@dx/dx/meta.json';
+const libName = '@dx/dx';
+const apiUrl = `https://jsr.io/${libName}/meta.json`;
 
-export const upgradeDx = async (dryRun: boolean, version?: string): Promise<number> => {
-  const dxVersion = parse(VERSION);
-  const denoVersion = parse(Deno.version.deno);
+export const upgradeDx = async (dryRun: boolean = true): Promise<number> => {
+  const dxVersion: SemVer = parse(VERSION);
   const response: Response = await fetch(apiUrl);
   const meta: JsrMeta = await response.json();
+  const latestDxVersion: SemVer = parse(meta.latest);
 
-  const args = ['install', '--allow-read', '--allow-run', '--allow-sys', '--allow-env', '--allow-write', '--unstable-kv', '-g', '-f', '-n', 'dx', 'jsr:@dx/dx'];
+  if (!greaterThan(latestDxVersion, dxVersion)) {
+    console.log(`%cYou are using the latest version: ${VERSION}`, 'color: green; font-weight: bold');
+    return 0;
+  }
+
+  console.log(`%cNew version available: ${meta.latest}`, 'color: green; font-weight: bold');
+
+  const args = ['i', '--allow-run', '-RSE', '--unstable-kv', '-g', '-f', '-n', 'dx', `jsr:${libName}@${latestDxVersion}`];
 
   return await runner.run(args, dryRun);
 };
